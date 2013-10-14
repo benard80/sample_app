a3cef77bac7d4c024eee979f299a6e7c71e9f9a1
benard80 <ben_4u_7@hotmail.com>
2013-10-14 14:01:21 -0300

    add user following

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,14 @@
 class User < ActiveRecord::Base
 
   has_many :microposts, dependent: :destroy
+  has_many :relationships, foreign_key: "follower_id", dependent: :destroy
+  has_many :followed_users, through: :relationships, source: :followed
+
+  has_many :reverse_relationships, foreign_key: "followed_id",
+                                   class_name: "Relationship",
+                                   dependent: :destroy
+
+  has_many  :followers, through: :reverse_relationships, source: :follower
 
 	before_save { self.email = email.downcase }
 	before_create :create_remember_token
@@ -23,6 +31,22 @@ class User < ActiveRecord::Base
      #this a preliminary. see followers for full implimenation
      Micropost.where("user_id= ?", id )
    end
+
+   def following?(other_user)
+    relationships.find_by(followed_id: other_user.id)
+    end
+
+    def follow!(other_user)
+      relationships.create!(followed_id: other_user.id)
+    end
+
+    def unfollow!(other_user)
+    relationships.find_by(followed_id: other_user.id).destroy!
+    end
+
+    def feed
+      Micropost.from_user_followed_by(self)
+    end
 
   private
 
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -27,6 +27,36 @@ describe "UserPages" do
       end
     end
 
+    describe "micropost associations" do
+    before { @user.save }
+    let!(:older_micropost) do
+      FactoryGirl.create(:micropost, user: @user, created_at: 1.day.ago)
+    end
+    let!(:newer_micropost) do
+      FactoryGirl.create(:micropost, user: @user, created_at: 1.hour.ago)
+    end
+    describe "status" do
+      let(:unfollowed_post) do
+        FactoryGirl.create(:micropost, user: FactoryGirl.create(:user))
+      end
+      let(:followed_user) { FactoryGirl.create(:user) }
+
+      before do
+        @user.follow!(followed_user)
+        3.times { followed_user.microposts.create!(content: "Lorem ipsum") }
+      end
+
+      its(:feed) { should include(newer_micropost) }
+      its(:feed) { should include(older_micropost) }
+      its(:feed) { should_not include(unfollowed_post) }
+      its(:feed) do
+        followed_user.microposts.each do |micropost|
+          should include(micropost)
+        end
+      end
+    end
+  end
+
     describe "delete links"do
      it { should_not have_link('delete') }
 
@@ -69,8 +99,59 @@ describe "UserPages" do
       it { should have_content(m2.content) }
       it { should have_content(user.microposts.count) }
     end
-  end
+
+    describe "follow/unfollow buttons" do
+      let(:other_user) { FactoryGirl.create(:user) }
+      before { sign_in user }
+
+      describe "following a user" do
+        before { visit user_path(other_user) }
+
+        it "should increment the followed user count" do
+          expect do
+            click_button "Follow"
+          end.to change(user.followed_users, :count).by(1)
+        end
+
+        it "should increment the other user's followers count" do
+          expect do
+            click_button "Follow"
+          end.to change(other_user.followers, :count).by(1)
+        end
+
+        describe "toggling the button" do
+          before { click_button "Follow" }
+          it { should have_xpath("//input[@value='Unfollow']") }
+        end
+      end
+
+      describe "unfollowing a user" do
+        before do
+          user.follow!(other_user)
+          visit user_path(other_user)
+        end
+
+        it "should decrement the followed user count" do
+          expect do
+            click_button "Unfollow"
+          end.to change(user.followed_users, :count).by(-1)
+        end
+
+        it "should decrement the other user's followers count" do
+          expect do
+            click_button "Unfollow"
+          end.to change(other_user.followers, :count).by(-1)
+        end
+
+        describe "toggling the button" do
+          before { click_button "Unfollow" }
+          it { should have_xpath("//input[@value='Follow']") }
+        end
+      end
+     end
+    end
   
+
   describe "signup" do
     before { visit signup_path }
 
@@ -124,7 +205,7 @@ describe "UserPages" do
 
       it { should have_content('error') }
     end
-    describe "with valid information" do
+     describe "with valid information" do
       let(:new_name)  { "Benard Pacho" }
       let(:new_email) { "benard@yahoo.com" }
       before do
@@ -142,4 +223,32 @@ describe "UserPages" do
       specify { expect(user.reload.email).to eq new_email }
     end
   end
-end
+
+  describe "following/followers" do
+    let(:user) { FactoryGirl.create(:user) }
+    let(:other_user) { FactoryGirl.create(:user) }
+    before { user.follow!(other_user) }
+
+    describe "followed users" do
+       before do
+        sign_in other_user
+        visit following_user_path(user)
+       end
+       it { should have_title(full_title('Following')) }
+       it { should have_selector('h3', text: "Following") }
+       it { should have_link(other_user.name, href: user_path(other_user)) }
+     end
+
+     describe "followers" do
+       before do
+         sign_in other_user
+         visit followers_user_path(other_user)
+        end
+
+       it { should have_title(full_title('Followers')) }
+       it { should have_selector('h3', text: "Followers") }
+       it { should have_link(user.name, href: user_path(user)) }
+      end
+    end
+  end
+ 
